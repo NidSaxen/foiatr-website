c57ecb39990e3b78778aee8af50fe7fb81a251dd
Your Name <yourname@example.com>
2026-02-24 22:37:10 +0300

    Fix malformed Unsplash links

--- data-slideshow.js
+++ data-slideshow.js
@@ -14,7 +14,7 @@ const slideshowData = [
         title: "Classical Indian Architecture"
     },
     {
-        image: "https://unsplash.com/photos/a-painting-of-a-man-and-woman-in-a-forest-5PehHWn75QIhttps://images.unsplash.com/photo-1713986719987-fb689460f1f8?q=80&w=2178&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://images.unsplash.com/photo-1713986719987-fb689460f1f8?q=80&w=1200&auto=format&fit=crop",
         title: "Vibrant Cultural Patterns"
     },
     {
@@ -22,7 +22,7 @@ const slideshowData = [
         title: "The Colors of India"
     },
     {
-        image: "https://images.unsplash.com/photo-1581836850314-3b668c2aa540?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://images.unsplash.com/photo-1581836850314-3b668c2aa540?q=80&w=1200&auto=format&fit=crop",
         title: "Classical Indian Architecture"
     }
 
