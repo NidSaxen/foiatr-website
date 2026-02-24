ca5c1ac96cc67bd47e42455608516b9354af9bdd
Your Name <yourname@example.com>
2026-02-24 22:31:53 +0300

    Update slideshow image

--- data-slideshow.js
+++ data-slideshow.js
@@ -14,11 +14,16 @@ const slideshowData = [
         title: "Classical Indian Architecture"
     },
     {
-        image: "https://images.unsplash.com/photo-1713986719987-fb689460f1f8?q=80&w=1200&auto=format&fit=crop",
+        image: "https://unsplash.com/photos/a-painting-of-a-man-and-woman-in-a-forest-5PehHWn75QIhttps://images.unsplash.com/photo-1713986719987-fb689460f1f8?q=80&w=2178&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "Vibrant Cultural Patterns"
     },
     {
         image: "https://images.unsplash.com/photo-1587595431973-160d0d94add1?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200&q=80",
         title: "The Colors of India"
+    },
+    {
+        image: "https://images.unsplash.com/photo-1581836850314-3b668c2aa540?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        title: "Classical Indian Architecture"
     }
+
 ];
